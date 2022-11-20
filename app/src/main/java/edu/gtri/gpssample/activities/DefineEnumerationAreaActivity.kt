2c669a8d1f37633e19d3b9472f434b498c77fb2c
Russell Mitchell <russell.mitchell@gtri.gatech.edu>
2022-11-20 12:09:38 -0600

    Added Google Maps

--- app/src/main/java/edu/gtri/gpssample/activities/DefineEnumerationAreaActivity.kt
+++ app/src/main/java/edu/gtri/gpssample/activities/DefineEnumerationAreaActivity.kt
@@ -5,12 +5,18 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import edu.gtri.gpssample.R
 import edu.gtri.gpssample.databinding.ActivityDefineEnumerationAreaBinding
 
-class DefineEnumerationAreaActivity : AppCompatActivity() {
+class DefineEnumerationAreaActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private lateinit var map: GoogleMap
     private lateinit var binding: ActivityDefineEnumerationAreaBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +24,10 @@ class DefineEnumerationAreaActivity : AppCompatActivity() {
 
         binding = ActivityDefineEnumerationAreaBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+
+        mapFragment!!.getMapAsync(this)
 
         binding.backButton.setOnClickListener {
             finish()
@@ -29,6 +39,16 @@ class DefineEnumerationAreaActivity : AppCompatActivity() {
             startActivity( intent )
             this.overridePendingTransition(R.animator.slide_from_right, R.animator.slide_to_left)
         }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        val srb = LatLng(30.330603,-86.165004 )
+
+        map.addMarker( MarkerOptions().position(srb).title("Grayton Beach, FL"))
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom( srb, 15.0f))
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
