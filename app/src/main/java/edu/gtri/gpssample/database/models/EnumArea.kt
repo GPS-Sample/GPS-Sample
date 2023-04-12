b186f9c8b8cf0146be9c33fe02ff667f31b3f3ed
Russell Mitchell <russell.mitchell@gtri.gatech.edu>
2023-04-12 16:29:02 -0500

    Add Household updates

--- app/src/main/java/edu/gtri/gpssample/database/models/EnumArea.kt
+++ app/src/main/java/edu/gtri/gpssample/database/models/EnumArea.kt
@@ -1,6 +1,9 @@
 package edu.gtri.gpssample.database.models
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 @Serializable
 data class EnumArea (
@@ -11,4 +14,22 @@ data class EnumArea (
 {
     constructor(id: Int, config_id: Int, name: String) : this(id, config_id, name, ArrayList<LatLon>())
     constructor(config_id: Int, name: String, vertices: ArrayList<LatLon>) : this(null, config_id, name, vertices)
+
+    fun copy() : EnumArea
+    {
+        return unpack(pack())
+    }
+
+    fun pack() : String
+    {
+        return Json.encodeToString( this )
+    }
+
+    companion object
+    {
+        fun unpack( string: String ) : EnumArea
+        {
+            return Json.decodeFromString<EnumArea>( string )
+        }
+    }
 }
