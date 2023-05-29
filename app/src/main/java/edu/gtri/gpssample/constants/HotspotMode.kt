784cbf9b44eea7c21bfd8cba7a48a7976907a93a
Brian Parise <brian.parise@gmail.com>
2023-05-29 16:39:48 -0400

    added qr code creation for supervisor

--- app/src/main/java/edu/gtri/gpssample/constants/HotspotMode.kt
+++ app/src/main/java/edu/gtri/gpssample/constants/HotspotMode.kt
@@ -2,8 +2,8 @@ package edu.gtri.gpssample.constants
 
 enum class HotspotMode(val format : String) {
     None("None"),
-    Configuration("Configuration Hotspot"),
-    EnumerationTeam("Enumeration Team Hotspot"),
+    Admin("Admin Hotspot"),
+    Supervisor("Supervisor Hotspot"),
 
 }
 
