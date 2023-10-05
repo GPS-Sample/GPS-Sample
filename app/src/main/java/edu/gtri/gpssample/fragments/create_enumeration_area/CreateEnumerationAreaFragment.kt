37c6f6681d277f280ebc99c22a6699cf1bab8beb
Russell Mitchell <russell.mitchell@gtri.gatech.edu>
2023-10-05 10:36:54 -0500

    fixed issue with saving collection data

--- app/src/main/java/edu/gtri/gpssample/fragments/create_enumeration_area/CreateEnumerationAreaFragment.kt
+++ app/src/main/java/edu/gtri/gpssample/fragments/create_enumeration_area/CreateEnumerationAreaFragment.kt
@@ -407,7 +407,7 @@ class CreateEnumerationAreaFragment : Fragment(),
     fun getAllEnumAreas() : ArrayList<EnumArea>
     {
         val allEnumAreas = ArrayList<EnumArea>()
-        allEnumAreas.addAll( config.enumAreas )
+        allEnumAreas.addAll( DAO.enumAreaDAO.getEnumAreas( config ) )
         allEnumAreas.addAll( unsavedEnumAreas )
         return allEnumAreas
     }
