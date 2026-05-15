f5ff1a1d6b734f9d0f47bc91032a36cf60d5a536
RussMitch <5808223+RussMitch@users.noreply.github.com>
2026-05-15 16:24:19 -0500

    String translation updates.

--- translations/translate/translate.py
+++ translations/translate/translate.py
@@ -31,7 +31,7 @@ def translate(appName, language, index):
   
 	# step 1: pull out all of the <string> elements
 	for i in range(1,lines.__len__()):
-		line = lines[i].rstrip('\r')
+		line = lines[i].rstrip('\r').replace("_N_", "\\n")
 
 		# split words separated by '\t'
 		components = line.split('\t')
