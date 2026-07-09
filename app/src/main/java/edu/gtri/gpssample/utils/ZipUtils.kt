c6dc7911bfa0e541bf88b6e4a307d4d971e8cba4
RussMitch <5808223+RussMitch@users.noreply.github.com>
2026-07-09 16:24:30 -0400

    Fixed issue with save to local storage on Android 8/9

--- app/src/main/java/edu/gtri/gpssample/utils/ZipUtils.kt
+++ app/src/main/java/edu/gtri/gpssample/utils/ZipUtils.kt
@@ -145,7 +145,7 @@ object ZipUtils
             {
                 val resolver = activity.contentResolver
                 val values = ContentValues().apply {
-                    put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, fileName + ".zip" )
                     put(MediaStore.MediaColumns.MIME_TYPE, "application/zip")
 
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
@@ -161,7 +161,7 @@ object ZipUtils
                             dir.mkdirs()
                         }
 
-                        val file = File(dir, fileName)
+                        val file = File(dir, fileName + ".zip" )
                         put(MediaStore.MediaColumns.DATA, file.absolutePath)
                     }
                 }
